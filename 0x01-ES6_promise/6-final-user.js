4728980b2f8db3bee862a50323c05cb69d212615
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2023-04-21 19:45:57 +0100

    fixing errors for second deadline

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,3 @@
-// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -9,9 +8,11 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const results = await Promise.allSettled([signUpUserPromise, uploadPhotoPromise]);
   for (const result of results) {
     const { status } = result;
-    const value = status === 'fulfilled' ? result.value : result.reason;
+    // console.log(result)
+    const value = status === 'fulfilled' ? result.value : result.reason.toString();
     // Push the result object to the resultArray
     resultArray.push({ status, value });
   }
+  // console.log(resultArray)
   return resultArray;
 }
